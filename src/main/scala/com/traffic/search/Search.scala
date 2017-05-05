c3476d00094b0c6f7c02d38f22c7b8455747ff55
WJustHM <249956445@qq.com>
2017-05-05 10:38:58 +0800

    traffic.searchqinshi

--- src/main/scala/com/traffic/search/Search.scala
+++ src/main/scala/com/traffic/search/Search.scala
@@ -2,8 +2,8 @@ package com.traffic.search
 
 import java.text.SimpleDateFormat
 import java.util._
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.util
+import java.util.concurrent.{Callable, ExecutorService, Executors, Future}
 
 import com.common.Pools
 import org.apache.hadoop.hbase.TableName
@@ -24,7 +24,7 @@ class Search extends Pools {
   val conn: HbaseConnection = getHbaseConn
   //连接已经建立
   val client = getEsClient
-  //连接以建立
+  //连接已建立
   val redis = initRedis
   val tablename = TableName.valueOf("Result")
   val gettable = conn.getTable(tablename)
@@ -34,14 +34,15 @@ class Search extends Pools {
   val DATASOURCE = "DATASOURCE"
   var num = 0;
 
+  //查询一
   def searchElasticHBase(vehicleBrand: String, PlateColor: String, Direction: String, tag: String
                          , paper: String, sun: String, drop: String
                          , secondBelt: String, crash: String, danger: String
                          , starttime: String, endtime: String
                         ): Unit = {
-    //    val stype = start.split(" ")(0).split("\\-")(1).toInt
-    //    val etype = end.split(" ")(0).split("\\-")(1).toInt
+    //建立ES索引连接
     val request: SearchRequestBuilder = client.prepareSearch().setIndices("vehicle").setTypes("result")
+    //ES查询Json代码
     val qu = "{\n" +
       "    \"bool\": {\n" +
       "      \"must\": [\n" +
@@ -93,30 +94,50 @@ class Search extends Pools {
       "            \"lte\": \"" + endtime + "\"\n" +
       "          }\n" + "        }\n" + "      }\n" + "    }\n" +
       "  }"
+    //执行查询语句
     var response: SearchResponse = request.setQuery(QueryBuilders.wrapperQuery(qu))
       .addSort("ResultTime", SortOrder.ASC).setScroll(new TimeValue(60000)).setSize(50).execute().actionGet()
     println("-----Search hit total data:" + response.getHits.getTotalHits.toString)
+    val fuList = new util.LinkedList[Future[util.Map[String, String]]]()
     do {
       val starttime = System.currentTimeMillis()
       for (rs <- response.getHits.getHits) {
-        Search.pool.submit(new Runnable {
-          override def run(): Unit = {
-            searchHBase(rs.getSource.get("resultId").toString)
+        //异步查询
+        val res = Search.pool.submit(new Callable[util.Map[String, String]] {
+          override def call(): util.Map[String, String] = {
+            val resultHBase = searchHBase(rs.getSource.get("resultId").toString)
+            val resultRdeis = searchRedis(TASK, rs.getSource.get("taskId").toString)
+            joinElasticHBase(resultHBase, resultRdeis)
+            resultHBase
           }
         })
-        Search.pool.submit(new Runnable {
-          override def run(): Unit = {
-            searchRedis(TASK, rs.getSource.get("taskId").toString)
-          }
-        })
-        num += 1
+        fuList.add(res)
       }
-      println(num)
+      //迭代查询结果
+      val d = fuList.iterator()
+      while (d.hasNext) {
+        d.next().get().get("HP")
+      }
+      fuList.clear()
+      //计算批次返回时间
       println("------------------------------Search Spend Time:" + (System.currentTimeMillis() - starttime))
       response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet()
     } while (response.getHits.getHits.length != 0)
   }
 
+  //查询结果
+  def joinElasticHBase(resHBase: Map[String, String], resRedis: Map[String, String]): Map[String, String] = {
+    val result = new HashMap[String, String]
+    result
+  }
+
+  //查询结果
+  def joinRedis(resRedistask: Map[String, String], resRediscamera: Map[String, String], resRedisdataSource: Map[String, String]): Map[String, String] = {
+    val result = new HashMap[String, String]
+    result
+  }
+
+  //执行Redis查询
   def searchRedis(table: String, id: String): HashMap[String, String] = {
     var map: HashMap[String, String] = null
     if (redis.hexists(table, id)) {
@@ -124,9 +145,9 @@ class Search extends Pools {
       map = mapper.readValue(result, new HashMap[String, String].getClass)
     }
     map
-
   }
 
+  //建立Redis连接
   def initRedis: JedisCluster = {
     val jedisClusterNodes = new HashSet[HostAndPort]()
     //在添加集群节点的时候只需要添加一个，其余同一集群的J节点会被自动加入
@@ -135,8 +156,9 @@ class Search extends Pools {
     jc
   }
 
-
-  def searchHBase(rw: String): Unit = {
+  //HBase查询代码
+  def searchHBase(rw: String): Map[String, String] = {
+    val resultHbase = new util.HashMap[String, String]()
     val get = new Get(rw.getBytes)
     val result = gettable.get(get)
     val License = Bytes.toString(result.getValue("Result".getBytes, "License".getBytes))
@@ -153,51 +175,79 @@ class Search extends Pools {
     val vehicleBrand = Bytes.toString(result.getValue("Result".getBytes, "vehicleBrand".getBytes))
     val vehicleStyle = Bytes.toString(result.getValue("Result".getBytes, "vehicleStyle".getBytes))
     val LocationLeft = Bytes.toString(result.getValue("Result".getBytes, "LocationLeft".getBytes))
+    resultHbase.put("HP", License)
+    resultHbase
   }
 
-  def searchCarnumber(start: String, end: String): Unit = {
-    val stype = start.split(" ")(0).split("\\-")(1).toInt
-    val etype = end.split(" ")(0).split("\\-")(1).toInt
+  //查询三
+  def searchCarnumber(starttime: String, endtime: String): Unit = {
+    //建立ES索引连接
     val request: SearchRequestBuilder = client.prepareSearch().setIndices("vehicle").setTypes("result")
+    //ES查询Json代码
     val qu = "{\n" +
       "    \"range\": {\n" +
       "      \"ResultTime\": {\n" +
-      "        \"gte\": \"" + start + "\",\n" +
-      "        \"lte\": \"" + end + "\"\n" +
+      "        \"gte\": \"" + starttime + "\",\n" +
+      "        \"lte\": \"" + endtime + "\"\n" +
       "      }\n" +
       "    }\n" +
       "  }"
+    //执行查询语句
     var response: SearchResponse = request.setQuery(QueryBuilders.wrapperQuery(qu)).execute().actionGet()
     println("-----Search hit total data:" + response.getHits.getTotalHits.toString)
-    val starttime = System.nanoTime()
+    val start = System.nanoTime()
     response.getHits.getTotalHits.toString
-    println("------------------------------Search Spend Time:" + (System.nanoTime() - starttime) / 1000)
+    println("------------------------------Search Spend Time:" + (System.nanoTime() - start) / 1000)
   }
 
-  def search4(start: String): Unit = {
+  //查询四
+  def searchLicense(starttime: String, province: String, regexnumber: String): Unit = {
     val simplehms = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     val now = simplehms.format(new Date(System.currentTimeMillis()))
-    val stype = start.split(" ")(0).split("\\-")(1).toInt
-    val etype = now.split(" ")(0).split("\\-")(1).toInt
     var num = 0
+    //建立ES索引连接
     val request: SearchRequestBuilder = client.prepareSearch().setIndices("vehicle").setTypes("result")
+    //ES查询Json代码
     val qu = "{\n" +
-      "    \"range\": {\n" +
-      "      \"ResultTime\": {\n" +
-      "        \"gte\": \"" + start + "\",\n" +
-      "        \"lte\": \"" + now + "\"\n" +
-      "      }\n" +
+      "    \"bool\": {\n" +
+      "      \"must\": [\n" +
+      "        {\n" +
+      "          \"regexp\": {\n" +
+      "            \"Plate.keyword\": {\n" +
+      "              \"value\": \"" + province + ".{5}" + regexnumber + "\"\n" +
+      "            }\n" +
+      "          }\n" +
+      "        },\n" +
+      "        {\n" +
+      "          \"range\": {\n" +
+      "            \"ResultTime\": {\n" +
+      "              \"gte\": \"" + starttime + "\",\n" +
+      "              \"lte\": \"" + now + "\"\n" +
+      "            }\n" +
+      "          }\n" +
+      "        }\n" +
+      "      ]\n" +
       "    }\n" +
       "  }"
-    var response: SearchResponse = request.setQuery(QueryBuilders.wrapperQuery(qu)).setSize(100)
+    //执行查询语句
+    var response: SearchResponse = request.setQuery(QueryBuilders.wrapperQuery(qu)).addSort("ResultTime", SortOrder.ASC).setSize(100)
       .setScroll(new TimeValue(60000)).execute().actionGet()
     println("-----Search hit total data:" + response.getHits.getTotalHits.toString)
+    val fuList = new util.LinkedList[Future[util.Map[String, String]]]()
     do {
       val starttime = System.currentTimeMillis()
       for (rs <- response.getHits.getHits) {
-        searchRedis(TASK, rs.getSource.get("taskId").toString)
-        searchRedis(CAMERA, rs.getSource.get("cameraId").toString)
-        searchRedis(DATASOURCE, rs.getSource.get("dataSourceId").toString)
+        //异步查询
+        val res = Search.pool.submit(new Callable[util.Map[String, String]] {
+          override def call(): util.Map[String, String] = {
+            val task = searchRedis(TASK, rs.getSource.get("taskId").toString)
+            val camera = searchRedis(CAMERA, rs.getSource.get("cameraId").toString)
+            val dataSource = searchRedis(DATASOURCE, rs.getSource.get("dataSourceId").toString)
+            joinRedis(task, camera, dataSource)
+            task
+          }
+        })
+        fuList.add(res)
       }
       println("------------------------------Search Spend Time:" + (System.currentTimeMillis() - starttime))
       response = client.prepareSearchScroll(response.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet()
@@ -206,13 +256,14 @@ class Search extends Pools {
 }
 
 object Search {
-  val pool: ExecutorService = Executors.newWorkStealingPool(5)
+  //创建线程池
+  val pool: ExecutorService = Executors.newWorkStealingPool(50)
 
   def main(args: Array[String]): Unit = {
     val sbe = new Search
-        sbe.searchElasticHBase("2017-04-10 15:37:02", "2017-04-20 15:37:02", "白", "大众")
+    //        sbe.searchElasticHBase("别克", "黄", "1", "true", "false", "false", "true", "true", "true", "false", "2017-04-10 15:37:02", "2017-04-20 15:37:02")
     //    sbe.searchCarnumber("2017-04-20 15:30:14", "2017-04-25 15:30:14")
-    //    sbe.search4("2017-04-20 15:30:14")
+    //    sbe.searchLicense("2017-04-20 15:30:14", "粤", "2")
 
   }
 }
